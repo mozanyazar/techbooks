5ceecda7dd4c7bd8854602ef1b0f7e56657bd9a4
mozanyazar <mahmutozanyazar@gmail.com>
2023-04-23 14:43:33 +0300

    photo upload has been moved to middleware

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -1,34 +1,11 @@
 import Blog from '../models/blogModel.js'
-import User from '../models/userModel.js'
 import catchAsync from '../utils/catchAsync.js'
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
-import path from 'path'
-import { v4 as uuidv4 } from 'uuid'
 
 export const createBlog = catchAsync(async (req, res) => {
-  const __filename = fileURLToPath(import.meta.url)
-  const __dirname = dirname(__filename)
-
-  const imageFile = req.files.image
-  const randomName = uuidv4()
-
-  const uploadPath = path.join(
-    __dirname,
-    '../uploads/images',
-    `${randomName}-${imageFile.name}`
-  )
-
-  const pathname = `${path.basename(uploadPath)}`
-
-  imageFile.mv(uploadPath, (err) => {
-    if (err) {
-      return res.status(500).json({ message: err })
-    }
-  })
-
+  const imagePath = req.files
   let data = JSON.parse(req.body.json)
-  data.image = pathname
+  data.image = imagePath
+  data.user = req.user
 
   const blog = await Blog.create(data)
   res.status(200).json({
