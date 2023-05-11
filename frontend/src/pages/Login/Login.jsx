2c392b5d797d2629c0d0ae8ee1254db1b562b877
mozanyazar <mahmutozanyazar@gmail.com>
2023-05-11 20:28:50 +0300

    optimized login and signup func & added loading state

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -4,10 +4,13 @@ import { Link, useNavigate } from 'react-router-dom'
 import { getUserError, login } from '../../store/userSlice'
 import { useDispatch, useSelector } from 'react-redux'
 import { forgotPassword } from '../../services/auth'
+import { getUserBasket } from '../../store/basketSlice'
+import Loading from '../../Loading/Loading'
 
 const Login = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(false)
   const err = useSelector(getUserError)
 
   // switch the forms (login || forgotpassword)
@@ -22,133 +25,158 @@ const Login = () => {
   // login
   async function handleSubmit(e) {
     e.preventDefault()
-    dispatch(login({ email, password })).then((res) => {
-      if (res.status === 'success') {
-        navigate('/')
-        // set input emty
-        setPassword('')
-        setEmail('')
+    try {
+      setLoading(true)
+      const response = await dispatch(login({ email, password }))
+      if (response.payload.status === 'fail') {
+        setLoading(false)
+        setResetMessage('Email or password is incorrect')
+        return setTimeout(() => {
+          setResetMessage('')
+        }, 2000)
       }
-    })
+      await dispatch(getUserBasket())
+      setLoading(false)
+      navigate('/')
+      setPassword('')
+      setEmail('')
+    } catch (error) {}
   }
 
   // forgot password
   async function forgotHandler(e) {
     e.preventDefault()
     try {
+      setLoading(true)
       const response = await forgotPassword({ email })
       setResetMessage(response.message)
-
-      if (response.status === 'success') {
-        setTimeout(() => {
-          navigate('/')
+      console.log(response)
+      if (response.status !== 'success') {
+        setLoading(false)
+        setResetMessage('Email not found!')
+        return setTimeout(() => {
+          setResetMessage('')
         }, 2000)
       }
+      setTimeout(() => {
+        setLoading(false)
+        navigate('/')
+      }, 2000)
     } catch (error) {}
     setEmail('')
   }
 
   return (
-    <main className={styles.container}>
-      <div className={styles.title}>
-        {!userForgotPassword ? <h1>Login</h1> : <h1>Forgot Password</h1>}
-      </div>
-      <div className={styles.formContainer}>
-        {!userForgotPassword ? (
-          <form
-            onSubmit={(e) => handleSubmit(e)}
-            className={styles.form}
-          >
-            <label>
-              {!email && err && <p className={styles.error}>{err}</p>}
-            </label>
-            <label htmlFor="email">
-              Email
-              <input
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                name="email"
-                type="email"
-              />
-            </label>{' '}
-            <label htmlFor="password">
-              Password
-              <input
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                name="email"
-                type="password"
-                className={`${
-                  password && password.length < 6 && `${styles.errorForm}`
-                }`}
-              />
-              {password && password.length < 6 && (
-                <p className={styles.error}>
-                  Your password must be at least 6 characters long
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <main className={styles.container}>
+          <div className={styles.title}>
+            {!userForgotPassword ? <h1>Login</h1> : <h1>Forgot Password</h1>}
+          </div>
+          <div className={styles.formContainer}>
+            {!userForgotPassword ? (
+              <form
+                onSubmit={(e) => handleSubmit(e)}
+                className={styles.form}
+              >
+                {resetMessage && (
+                  <p className={styles.errorMessage}>{resetMessage}</p>
+                )}
+                <label>
+                  {!email && err && <p className={styles.error}>{err}</p>}
+                </label>
+                <label htmlFor="email">
+                  Email
+                  <input
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    name="email"
+                    type="email"
+                    autoComplete="true"
+                  />
+                </label>{' '}
+                <label htmlFor="password">
+                  Password
+                  <input
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    name="email"
+                    type="password"
+                    className={`${
+                      password && password.length < 6 && `${styles.errorForm}`
+                    }`}
+                  />
+                  {password && password.length < 6 && (
+                    <p className={styles.error}>
+                      Your password must be at least 6 characters long
+                    </p>
+                  )}
+                </label>{' '}
+                <div>
+                  <button
+                    disabled={password && password.length < 6}
+                    type="submit"
+                    className={styles.button}
+                  >
+                    Login
+                  </button>
+                  <button
+                    className={styles.button}
+                    onClick={() => {
+                      setEmail('')
+                      setPassword('')
+                      setUserForgotPassword(true)
+                    }}
+                  >
+                    Forgot Password
+                  </button>
+                </div>
+                <p className={styles.link}>
+                  You do not have an account? <Link to="/signup">Signup</Link>
                 </p>
-              )}
-            </label>{' '}
-            <div>
-              <button
-                disabled={password && password.length < 6}
-                type="submit"
-                className={styles.button}
+              </form>
+            ) : (
+              <form
+                onSubmit={(e) => forgotHandler(e)}
+                className={`${styles.form} ${styles.forgotForm}`}
               >
-                Login
-              </button>
-              <button
-                className={styles.button}
-                onClick={() => {
-                  setEmail('')
-                  setPassword('')
-                  setUserForgotPassword(true)
-                }}
-              >
-                Forgot Password
-              </button>
-            </div>
-            <p className={styles.link}>
-              You do not have an account? <Link to="/signup">Signup</Link>
-            </p>
-          </form>
-        ) : (
-          <form
-            onSubmit={(e) => forgotHandler(e)}
-            className={`${styles.form} ${styles.forgotForm}`}
-          >
-            {!email && resetMessage && (
-              <p className={styles.errorMessage}>{resetMessage}</p>
+                {resetMessage && (
+                  <p className={styles.errorMessage}>{resetMessage}</p>
+                )}
+                <label htmlFor="email">
+                  Email
+                  <input
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    name="email"
+                    type="email"
+                  />
+                </label>{' '}
+                <button
+                  type="submit"
+                  className={styles.button}
+                >
+                  Send mail
+                </button>
+                <button
+                  className={styles.button}
+                  onClick={(e) => {
+                    e.preventDefault()
+                    setEmail('')
+                    setPassword('')
+                    setUserForgotPassword(false)
+                  }}
+                >
+                  Back
+                </button>
+              </form>
             )}
-            <label htmlFor="email">
-              Email
-              <input
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                name="email"
-                type="email"
-              />
-            </label>{' '}
-            <button
-              type="submit"
-              className={styles.button}
-            >
-              Send mail
-            </button>
-            <button
-              className={styles.button}
-              onClick={(e) => {
-                e.preventDefault()
-                setEmail('')
-                setPassword('')
-                setUserForgotPassword(false)
-              }}
-            >
-              Back
-            </button>
-          </form>
-        )}
-      </div>
-    </main>
+          </div>
+        </main>
+      )}
+    </>
   )
 }
 
--- frontend/src/pages/Signup/Signup.jsx
+++ frontend/src/pages/Signup/Signup.jsx
@@ -7,6 +7,8 @@ import { createUserThunk, getUserError } from '../../store/userSlice'
 import { useDispatch, useSelector } from 'react-redux'
 import { AiOutlineSelect } from 'react-icons/ai'
 import { FaUserCircle } from 'react-icons/fa'
+import Loading from '../../Loading/Loading'
+import { getUserBasket } from '../../store/basketSlice'
 
 const Signup = () => {
   const navigate = useNavigate()
@@ -15,9 +17,11 @@ const Signup = () => {
   const [img, setImg] = useState(null)
   const [imgPreview, setImagePreview] = useState('')
   const [message, setMessage] = useState('')
+  const [loading, setLoading] = useState(false)
 
   const formSubmit = async (values, actions) => {
     if (!img) return setMessage('Photo is required')
+    setLoading(true)
 
     const formData = new FormData()
 
@@ -33,13 +37,18 @@ const Signup = () => {
     )
 
     // create user and get token from backend as a cookie
-    dispatch(createUserThunk(formData)).then((e) => {
-      if (e.payload.status === 'error') {
-        return
-      } else {
-        navigate('/')
-      }
-    })
+    const res = await dispatch(createUserThunk(formData))
+    console.log(res)
+    if (res.error) {
+      setLoading(false)
+      setMessage(res.error.message)
+      return setTimeout(() => {
+        setMessage('')
+      }, 1500)
+    }
+
+    await dispatch(getUserBasket())
+    navigate('/')
     actions.resetForm()
   }
   const validationSchema = yup.object().shape({
@@ -77,106 +86,116 @@ const Signup = () => {
     }
   }
   return (
-    <main className={styles.container}>
-      <div className={styles.title}>
-        <h1>Signup</h1>
-      </div>
-      <div className={styles.formContainer}>
-        <form
-          onSubmit={handleSubmit}
-          className={styles.form}
-        >
-          <label>
-            {!values.name && error && <p className={styles.error}>{error}</p>}
-          </label>
-          <label>{message && <p className={styles.error}>{message}</p>}</label>
-          <label
-            htmlFor="image"
-            className={styles.choosePhoto}
-          >
-            <input
-              type="file"
-              onChange={handleFileInputChange}
-              accept=".png, .jpg, .jpeg, .webp"
-            />
-            {imgPreview ? (
-              <img
-                style={{ maxWidth: '100%' }}
-                src={imgPreview}
-                alt="place"
-              />
-            ) : (
-              <>
-                <AiOutlineSelect className={styles.selectIcon} />
-                <FaUserCircle
-                  size={100}
-                  color="rgb(27 56 100)"
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <main className={styles.container}>
+          <div className={styles.title}>
+            <h1>Signup</h1>
+          </div>
+          <div className={styles.formContainer}>
+            <form
+              onSubmit={handleSubmit}
+              className={styles.form}
+            >
+              <label>
+                {!values.name && error && (
+                  <p className={styles.error}>{error}</p>
+                )}
+              </label>
+              <label>
+                {message && <p className={styles.errorMess}>{message}</p>}
+              </label>
+              <label
+                htmlFor="image"
+                className={styles.choosePhoto}
+              >
+                <input
+                  type="file"
+                  onChange={handleFileInputChange}
+                  accept=".png, .jpg, .jpeg, .webp"
                 />
-              </>
-            )}
-          </label>
-          <label htmlFor="name">
-            Name
-            <input
-              onChange={handleChange}
-              value={values.name}
-              name="name"
-              type="text"
-            />
-          </label>
-          <label htmlFor="email">
-            Email
-            <input
-              onChange={handleChange}
-              value={values.email}
-              name="email"
-              type="email"
-            />
-          </label>{' '}
-          <label htmlFor="password">
-            Password
-            <input
-              onChange={handleChange}
-              value={values.password}
-              name="password"
-              type="password"
-              className={`${
-                values.password && errors.password && `${styles.errorForm}`
-              } `}
-            />
-            {values.password && errors.password && (
-              <p className={styles.error}>{errors.password}</p>
-            )}
-          </label>
-          <label htmlFor="password">
-            Password Confirm
-            <input
-              onChange={handleChange}
-              value={values.passwordConfirm}
-              name="passwordConfirm"
-              type="password"
-              className={`${
-                values.passwordConfirm &&
-                errors.passwordConfirm &&
-                `${styles.errorForm}`
-              } `}
-            />
-            {values.passwordConfirm && errors.passwordConfirm && (
-              <p className={styles.error}>{errors.passwordConfirm}</p>
-            )}
-          </label>
-          <button
-            type="submit"
-            className={styles.button}
-          >
-            Signup
-          </button>
-          <p className={styles.link}>
-            Already have an account? <Link to="/login">Login</Link>
-          </p>
-        </form>
-      </div>
-    </main>
+                {imgPreview ? (
+                  <img
+                    style={{ maxWidth: '100%' }}
+                    src={imgPreview}
+                    alt="place"
+                  />
+                ) : (
+                  <>
+                    <AiOutlineSelect className={styles.selectIcon} />
+                    <FaUserCircle
+                      size={100}
+                      color="rgb(27 56 100)"
+                    />
+                  </>
+                )}
+              </label>
+              <label htmlFor="name">
+                Name
+                <input
+                  onChange={handleChange}
+                  value={values.name}
+                  name="name"
+                  type="text"
+                />
+              </label>
+              <label htmlFor="email">
+                Email
+                <input
+                  onChange={handleChange}
+                  value={values.email}
+                  name="email"
+                  type="email"
+                />
+              </label>{' '}
+              <label htmlFor="password">
+                Password
+                <input
+                  onChange={handleChange}
+                  value={values.password}
+                  name="password"
+                  type="password"
+                  className={`${
+                    values.password && errors.password && `${styles.errorForm}`
+                  } `}
+                />
+                {values.password && errors.password && (
+                  <p className={styles.error}>{errors.password}</p>
+                )}
+              </label>
+              <label htmlFor="password">
+                Password Confirm
+                <input
+                  onChange={handleChange}
+                  value={values.passwordConfirm}
+                  name="passwordConfirm"
+                  type="password"
+                  className={`${
+                    values.passwordConfirm &&
+                    errors.passwordConfirm &&
+                    `${styles.errorForm}`
+                  } `}
+                />
+                {values.passwordConfirm && errors.passwordConfirm && (
+                  <p className={styles.error}>{errors.passwordConfirm}</p>
+                )}
+              </label>
+              <button
+                type="submit"
+                className={styles.button}
+              >
+                Signup
+              </button>
+              <p className={styles.link}>
+                Already have an account? <Link to="/login">Login</Link>
+              </p>
+            </form>
+          </div>
+        </main>
+      )}
+    </>
   )
 }
 
