760e967699698740804ba2b53a2b0e2bcdaa29a4
mozanyazar <mahmutozanyazar@gmail.com>
2023-05-31 15:33:24 +0300

    footer form message added

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Footer.module.css'
 import photo from '../../images/Vector.png'
 import p1 from '../../images/p1.png'
@@ -8,6 +8,15 @@ import p4 from '../../images/p4.png'
 import { BsGithub } from 'react-icons/bs'
 import { AiFillLinkedin } from 'react-icons/ai'
 const Footer = () => {
+  const [mess, setMess] = useState('')
+
+  function keepInTouchHandler(e) {
+    e.preventDefault()
+    setMess('The message has been received')
+    setTimeout(() => {
+      setMess('')
+    }, 2000)
+  }
   return (
     <footer className={styles.footer}>
       <div className={styles.footerInner}>
@@ -21,25 +30,25 @@ const Footer = () => {
               <a href="/">Pages</a>
             </div>
             <div className={styles.footerSocials}>
-              <a href="#">
+              <a>
                 <img
                   src={p1}
                   alt="p1"
                 />
               </a>
-              <a href="#">
+              <a>
                 <img
                   src={p2}
                   alt="p2"
                 />
               </a>
-              <a href="#">
+              <a>
                 <img
                   src={p3}
                   alt="p3"
                 />
               </a>
-              <a href="#">
+              <a>
                 <img
                   src={p4}
                   alt="p4"
@@ -73,9 +82,16 @@ const Footer = () => {
               </div>
             </div>
           </div>
-          <form className={styles.form}>
-            <textarea placeholder="Keep in touch"></textarea>
-            <button>submit</button>
+          <form
+            onSubmit={keepInTouchHandler}
+            className={styles.form}
+          >
+            {mess ? (
+              <div className={styles.mess}>{mess}</div>
+            ) : (
+              <textarea placeholder="Keep in touch"></textarea>
+            )}
+            <button type="submit">submit</button>
           </form>
         </div>
         <div className={styles.footerBottom}>
